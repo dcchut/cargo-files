c3f4b759484d56291768b497b012de8a76468c6c
Robert Usher <266585+dcchut@users.noreply.github.com>
2023-03-11 12:24:39 +1100

    fix up macros so we're asserting from the right function

--- cargo-files-core/tests/tests.rs
+++ cargo-files-core/tests/tests.rs
@@ -1,28 +1,38 @@
+use tempfile::TempDir;
+
+macro_rules! set_snapshot_suffix {
+    ($($expr:expr),*) => {
+        let mut settings = insta::Settings::clone_current();
+        settings.set_snapshot_suffix(format!($($expr,)*));
+        let _guard = settings.bind_to_scope();
+    }
+}
+
+fn run_test(krate: &TempDir) -> String {
+    let crate_root = dunce::canonicalize(krate.path()).unwrap();
+    let projects = cargo_files_core::get_projects(Some(&crate_root.join("Cargo.toml"))).unwrap();
+
+    let mut paths = Vec::new();
+    for target in projects {
+        let files = cargo_files_core::get_target_files(&target).unwrap();
+        for file in files {
+            let relative_path = pathdiff::diff_paths(&file, &crate_root).unwrap();
+            let components: Vec<_> = relative_path
+                .components()
+                .map(|c| c.as_os_str().to_string_lossy().into_owned())
+                .collect();
+            paths.push(components.join("/"));
+        }
+    }
+    paths.sort();
+    paths.join("\n")
+}
+
 /// Generate a test case which detects which files are present in a crate.
 macro_rules! krate {
     ($def:literal) => {
         let krate = ::cargo_files_test::make_crate!($def);
-
-        let crate_root = dunce::canonicalize(krate.path()).unwrap();
-        let projects =
-            ::cargo_files_core::get_projects(Some(&crate_root.join("Cargo.toml"))).unwrap();
-
-        let mut paths = ::std::vec::Vec::new();
-
-        for target in projects {
-            let files = ::cargo_files_core::get_target_files(&target).unwrap();
-            for file in files {
-                let relative_path = ::pathdiff::diff_paths(&file, &crate_root).unwrap();
-                let components: Vec<_> = relative_path
-                    .components()
-                    .map(|c| c.as_os_str().to_string_lossy().into_owned())
-                    .collect();
-                paths.push(components.join("/"));
-            }
-        }
-
-        paths.sort();
-        ::insta::assert_snapshot!(paths.join("\n"));
+        insta::assert_snapshot!(run_test(&krate));
     };
 }
 
